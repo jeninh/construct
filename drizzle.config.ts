21609846bfc1027204a779326bfd723cef56a821
Arca Ege Cengiz <arcaegecengiz@gmail.com>
2025-11-25 19:40:00 +0000

    Switched to using postgres connection strings instead

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_HOST) throw new Error('DATABASE_HOST is not set');
-
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'postgresql',
 	dbCredentials: {
-		host: process.env.DATABASE_HOST,
-		port: 5432,
-		user: 'postgres',
-		ssl: false,
-		database: 'postgres'
+		url: process.env.DATABASE_URL!
 	},
 	verbose: true,
 	strict: true
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,16 +1,9 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
-
-if (!env.DATABASE_HOST) throw new Error('DATABASE_HOST is not set');
 
 const pool = new Pool({
-	host: process.env.DATABASE_HOST,
-	port: 5432,
-	user: 'postgres',
-	ssl: false,
-	database: 'postgres'
+	connectionString: process.env.DATABASE_URL!,
 });
 
 export const db = drizzle(pool, { schema });
